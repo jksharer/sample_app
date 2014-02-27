1cc00a3221c58f77ee0e8b6c61f74e48ea69c9ca
zhangkai <zk.amazing@gmail.com>
2014-02-27 16:12:52 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
+  resources :users
   
   root to: 'static_pages#home'
   match '/', to: 'static_pages#home', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  
   
   match '/signup', to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "zhangkai"
+    email     "zhangkai@163.com"
+    password  "123456"
+    password_confirmation  "123456"
+  end
+end
