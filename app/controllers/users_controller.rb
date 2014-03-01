5dc257f1c65b0b61ad8038375d1a34d4fba0dda8
zhangkai <zk.amazing@gmail.com>
2014-03-02 02:50:32 +0800

    change new to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
       flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'new'  
+      render 'edit'  
     end
   end
   
